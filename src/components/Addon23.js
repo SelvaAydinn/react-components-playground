2129e9fe74ecc38980c1e79399d5687b9fa3dd0f
SelvaAydinn <selvaaydin972@gmail.com>
2019-08-01 11:44:00 +0300

    23

--- src/components/Addon23.js
+++ src/components/Addon23.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react';
 import FiddleComponent from "./FiddleComponent";
+import InputGroup from 'react-bootstrap/InputGroup';
+
+
+const scope = {InputGroup};
+
 
 class Addon23 extends Component {
     render() {
         return (
             <div>
-                <FiddleComponent code="<div>this is Addon23</div>" ></FiddleComponent>
-               SELVA A
+                <FiddleComponent code='<div>
+                                           <InputGroup size="sm" >
+                                             <p>  -InputGroup.x aria-label="x for following text input"- x=Checkbox or x=Radio</p>
+                                                  <InputGroup.Prepend>
+                                                     <InputGroup.Checkbox aria-label="Checkbox for following text input" />
+                                                  </InputGroup.Prepend>
+                                           </InputGroup> </div>' scope={scope} >
+                </FiddleComponent>
             </div>
         );
     }
